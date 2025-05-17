22b79d2b0fa3b65f1cc1602ff7db623df7af972d
Mike Klein <mtklein@gmail.com>
2025-05-17 08:24:32 +0800

    sub,mul

--- iv.c
+++ iv.c
@@ -1,8 +1,29 @@
 #include "iv.h"
+#include <math.h>
+
+static iv iv_containing(float a, float b, float c, float d) {
+    // TODO: can this be done in fewer comparisons?
+    return (iv) {
+        fminf(fminf(a,b), fminf(c,d)),
+        fmaxf(fmaxf(a,b), fmaxf(c,d)),
+    };
+}
 
 iv iv_add(iv x, iv y) {
     return (iv){
         x.lo + y.lo,
         x.hi + y.hi,
     };
+}
+
+iv iv_sub(iv x, iv y) {
+    return (iv){
+        x.lo - y.hi,
+        x.hi - y.lo,
+    };
+}
+
+iv iv_mul(iv x, iv y) {
+    return iv_containing(x.lo * y.lo, x.hi * y.lo,
+                         x.lo * y.hi, x.hi * y.hi);
 }
--- iv_test.c
+++ iv_test.c
@@ -9,13 +9,44 @@ static _Bool equiv(float x, float y) {
         || (x != x && y != y);
 }
 
-static void test_basics(void) {
+static void test_add(void) {
     iv z = iv_add((iv){3,4}, (iv){5,6});
     expect(equiv(z.lo,  8));
     expect(equiv(z.hi, 10));
 }
 
+static void test_sub(void) {
+    iv z = iv_sub((iv){3,4}, (iv){5,6});
+    expect(equiv(z.lo, -3));
+    expect(equiv(z.hi, -1));
+}
+
+static void test_mul(void) {
+    {
+        iv z = iv_mul((iv){3,4}, (iv){5,6});
+        expect(equiv(z.lo, 15));
+        expect(equiv(z.hi, 24));
+    }
+    {
+        iv z = iv_mul((iv){-3,4}, (iv){5,6});
+        expect(equiv(z.lo, -18));
+        expect(equiv(z.hi,  24));
+    }
+    {
+        iv z = iv_mul((iv){-3,4}, (iv){-5,6});
+        expect(equiv(z.lo, -20));
+        expect(equiv(z.hi,  24));
+    }
+    {
+        iv z = iv_mul((iv){-3,4}, (iv){-5,-1});
+        expect(equiv(z.lo, -20));
+        expect(equiv(z.hi,  15));
+    }
+}
+
 int main(void) {
-    test_basics();
+    test_add();
+    test_sub();
+    test_mul();
     return 0;
 }
