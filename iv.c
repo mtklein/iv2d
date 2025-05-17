26fe3d62294aa66968031936a49ce121aa666d64
Mike Klein <mtklein@gmail.com>
2025-05-17 08:41:16 +0800

    neither qwen3:14b nor I see a good way to improve iv_containing()

--- iv.c
+++ iv.c
@@ -2,7 +2,6 @@
 #include <math.h>
 
 static iv iv_containing(float a, float b, float c, float d) {
-    // TODO: can this be done in fewer comparisons?
     return (iv) {
         fminf(fminf(a,b), fminf(c,d)),
         fmaxf(fmaxf(a,b), fmaxf(c,d)),
