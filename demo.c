59f9245a6d7b9f36e2bc6148a10c9b861c042279
Mike Klein <mtklein@gmail.com>
2025-05-21 10:59:58 +0800

    a window with some thrilling content

--- demo.c
+++ demo.c
@@ -2,35 +2,67 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-SDL_AppResult SDL_AppInit(void **app, int argc, char *argv[]) {
+typedef struct {
+    SDL_Window   *window;
+    SDL_Renderer *renderer;
+} App;
+
+SDL_AppResult SDL_AppInit(void **ctx, int argc, char *argv[]) {
     (void)argc;
     (void)argv;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        return SDL_APP_FAILURE;
+    }
 
-    *app = NULL;
+    App *app = *ctx = SDL_malloc(sizeof *app);
+
+    if (!SDL_CreateWindowAndRenderer("iv2d demo", 800, 600, SDL_WINDOW_RESIZABLE,
+                                     &app->window, &app->renderer)) {
+        SDL_free(app);
+        SDL_Quit();
+        return SDL_APP_FAILURE;
+    }
+
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *app, SDL_AppResult res) {
-    (void)app;
+void SDL_AppQuit(void *ctx, SDL_AppResult res) {
+    App *app = ctx;
     (void)res;
+
+    SDL_DestroyRenderer(app->renderer);
+    SDL_DestroyWindow  (app->window);
+    SDL_free(app);
+    SDL_Quit();
 }
 
-SDL_AppResult SDL_AppEvent(void *app, SDL_Event *event) {
-    (void)app;
-
+SDL_AppResult SDL_AppEvent(void *ctx, SDL_Event *event) {
+    (void)ctx;
     switch (event->type) {
-        case SDL_EVENT_QUIT:
-            return SDL_APP_SUCCESS;
-        case SDL_EVENT_KEY_DOWN:
-            break;
-        default:
-            break;
+        case SDL_EVENT_QUIT: return SDL_APP_SUCCESS;
+        default:             break;
     }
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *app) {
-    (void)app;
+SDL_AppResult SDL_AppIterate(void *ctx) {
+    App *app = ctx;
+
+    int w,h;
+    SDL_GetRenderOutputSize(app->renderer, &w,&h);
+
+    SDL_FRect const square = {
+        (float)(w - 100) / 2,
+        (float)(h - 100) / 2,
+        100.0f,
+        100.0f,
+    };
+
+    SDL_SetRenderDrawColorFloat(app->renderer, 1,1,1,1);
+    SDL_RenderClear            (app->renderer         );
+    SDL_SetRenderDrawColorFloat(app->renderer, 0,0,0,1);
+    SDL_RenderFillRect         (app->renderer, &square);
+    SDL_RenderPresent          (app->renderer         );
 
     return SDL_APP_CONTINUE;
 }
