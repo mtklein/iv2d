280a84e63dc1bac2e9abb49215cce660d78b9749
Mike Klein <mtklein@gmail.com>
2025-05-24 11:48:40 +0800

    add batching

--- demo.c
+++ demo.c
@@ -4,25 +4,30 @@
 #include <SDL3/SDL_main.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     SDL_Window   *window;
     SDL_Renderer *renderer;
     int           mode,unused;
+
+    SDL_FRect *part,*full;
+    float     *part_cov;
+    int        parts, part_cap;
+    int        fulls, full_cap;
 } App;
 
 SDL_AppResult SDL_AppInit(void **ctx, int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         return SDL_APP_FAILURE;
     }
 
-    App *app = *ctx = SDL_calloc(1, sizeof *app);
+    App *app = *ctx = calloc(1, sizeof *app);
+    app->mode = argc > 1 ? atoi(argv[1]) : 0;
 
     if (!SDL_CreateWindowAndRenderer("iv2d demo", 800, 600, SDL_WINDOW_RESIZABLE,
                                      &app->window, &app->renderer)) {
-        SDL_free(app);
+        free(app);
         SDL_Quit();
         return SDL_APP_FAILURE;
     }
@@ -37,7 +42,10 @@ void SDL_AppQuit(void *ctx, SDL_AppResult res) {
 
     SDL_DestroyRenderer(app->renderer);
     SDL_DestroyWindow  (app->window);
-    SDL_free(app);
+    free(app->full);
+    free(app->part);
+    free(app->part_cov);
+    free(app);
     SDL_Quit();
 }
 
@@ -76,30 +84,43 @@ static iv circle_edge(struct circle c, iv x, iv y) {
                   iv_(c.r*c.r));
 }
 
-static void render_circle(SDL_Renderer *renderer, struct circle c, int l, int t, int r, int b) {
+static void push_coverage(App *app, int l, int t, int r, int b, float cov) {
+    SDL_FRect const rect = { (float)l, (float)t, (float)(r-l), (float)(b-t) };
+    if (cov == 1.0f) {
+        if (app->fulls == app->full_cap) {
+            app->full_cap = app->full_cap ? 2*app->full_cap : 1;
+            app->full = realloc(app->full, (size_t)app->full_cap * sizeof *app->full);
+        }
+        app->full[app->fulls++] = rect;
+    } else {
+        if (app->parts == app->part_cap) {
+            app->part_cap = app->part_cap ? 2*app->part_cap : 1;
+            app->part_cov = realloc(app->part_cov, (size_t)app->part_cap * sizeof *app->part_cov);
+            app->part     = realloc(app->part    , (size_t)app->part_cap * sizeof *app->part    );
+        }
+        app->part_cov[app->parts  ] = cov;
+        app->part    [app->parts++] = rect;
+    }
+}
+
+static void cover_circle(App *app, struct circle c, int l, int t, int r, int b) {
     iv const edge = circle_edge(c, (iv){(float)l, (float)r}
                                  , (iv){(float)t, (float)b});
 
     if (edge.lo < 0 && edge.hi < 0) {
-        // This rect has full coverage, drawn blue.
-        SDL_FRect const rect = {(float)l, (float)t, (float)(r-l), (float)(b-t)};
-        SDL_SetRenderDrawColor(renderer, 138,145,247,255);
-        SDL_RenderFillRect    (renderer, &rect);
+        push_coverage(app, l,t,r,b, 1.0f);
     }
     if (edge.lo < 0 && edge.hi >= 0) {
         int const x = (l+r)/2,
                   y = (t+b)/2;
         if (l == x && t == y) {
-            // This pixel has partial coverage, drawn green.
-            SDL_FRect const rect = {(float)l, (float)t, 1,1};
-            SDL_SetRenderDrawColor(renderer, 97,175,75,255);
-            SDL_RenderFillRect    (renderer, &rect);
+            push_coverage(app, l,t,r,b, 0.5f/*TODO*/);
         } else {
             // This rect has partial coverage, split and recurse.
-            render_circle(renderer, c, l,t, x,y);
-            render_circle(renderer, c, l,y, x,b);
-            render_circle(renderer, c, x,t, r,y);
-            render_circle(renderer, c, x,y, r,b);
+            cover_circle(app, c, l,t, x,y);
+            cover_circle(app, c, l,y, x,b);
+            cover_circle(app, c, x,t, r,y);
+            cover_circle(app, c, x,y, r,b);
         }
     }
 }
@@ -121,9 +142,15 @@ SDL_AppResult SDL_AppIterate(void *ctx) {
     uint64_t const freq = SDL_GetPerformanceFrequency(),
                   start = SDL_GetPerformanceCounter();
 
+    struct { uint8_t r,g,b; } full, part;
+    app->fulls = app->parts = 0;
     if (app->mode) {
-        render_circle(app->renderer, c, 0,0,w,h);
+        full.r = 138; full.g = 145; full.b = 247;
+        part.r =  97; part.g = 175; part.b =  75;
+        cover_circle(app, c, 0,0,w,h);
     } else {
+        full.r = 155; full.g = 155; full.b = 155;
+        part.r = 203; part.g = 137; part.b = 135;
         for (int y = 0; y < h; y++)
         for (int x = 0; x < w; x++) {
             float const fx = (float)x,
@@ -131,25 +158,24 @@ SDL_AppResult SDL_AppIterate(void *ctx) {
             iv const edge = circle_edge(c, (iv){fx,fx+1}
                                          , (iv){fy,fy+1});
             if (edge.lo < 0 && edge.hi < 0) {
-                // Full coverage pixel, drawn grey.
-                SDL_FRect const px = {fx,fy,1,1};
-                SDL_SetRenderDrawColor(app->renderer, 155,155,155,255);
-                SDL_RenderFillRect    (app->renderer, &px);
+                push_coverage(app, x,y,x+1,y+1, 1.0f);
             }
             if (edge.lo < 0 && edge.hi >= 0) {
-                // Partial coverage pixel, drawn red.
-                SDL_FRect const px = {fx,fy,1,1};
-                SDL_SetRenderDrawColor(app->renderer, 203,137,135,255);
-                SDL_RenderFillRect    (app->renderer, &px);
+                push_coverage(app, x,y,x+1,y+1, 0.5f/*TODO*/);
             }
         }
     }
-    uint64_t const elapsed = SDL_GetPerformanceCounter() - start;
+    SDL_SetRenderDrawColor(app->renderer, full.r, full.g, full.b, 255);
+    SDL_RenderFillRects   (app->renderer, app->full, app->fulls);
+    SDL_SetRenderDrawColor(app->renderer, part.r, part.g, part.b, 255);
+    SDL_RenderFillRects   (app->renderer, app->part, app->parts);
 
+    uint64_t const elapsed = SDL_GetPerformanceCounter() - start;
     char txt[128];
-    snprintf(txt, sizeof txt, "%d %.0fµs", app->mode, (double)elapsed * 1e6 / (double)freq);
-    SDL_SetRenderDrawColorFloat(app->renderer, 0,0,0,1);
-    SDL_RenderDebugText        (app->renderer, 0,4,txt);
+    snprintf(txt, sizeof txt, "%d %d full %d part %.0fµs",
+             app->mode, app->fulls, app->parts, (double)elapsed * 1e6 / (double)freq);
+    SDL_SetRenderDrawColor(app->renderer, 0,0,0,255);
+    SDL_RenderDebugText   (app->renderer, 0,4, txt);
 
     SDL_RenderPresent(app->renderer);
     return SDL_APP_CONTINUE;
