4edd3dd7c02356c6c86e7065cbfde88632bc1bd1
Mike Klein <mtklein@gmail.com>
2025-05-27 19:28:09 +0800

    rebalance comments, asserts, name, style

--- iv2d.c
+++ iv2d.c
@@ -1,73 +1,50 @@
 #include "iv2d.h"
 #include <assert.h>
 
-// Transform the result of evaluating an iv2d_region into an inside/outside coverage decision.
-// Remember, the general idea is that the expression is negative inside the region.
-// I'm not exactly sure how I want to treat exact 0, or even perhaps if ±0 should be treated
-// differently, but for now I try to consistently treat 0 as non-negative.
-static enum {NONE,FULL,MAYBE} coverage(iv R) {
-    assert(R.lo <= R.hi);
-    if (R.hi <  0) { return FULL; } // [-a, -b]                think, "all negative"
-    if (R.lo >= 0) { return NONE; } // [+a, +b] or [ 0, +b] or [0,0]  "non-negative"
-    assert(R.lo < 0 && 0 <= R.hi);  // [-a, +b] or [-a,  0]          "some negative"
-    return MAYBE;
+// Our core idea is that a region function R=fn(X,Y) is negative inside the region or
+// positive outside it.  By convention we treat an exact 0 (on the edge) as outside.
+// So all-negative means inside, all-non-negative outside, and a mix is uncertain.
+static enum {OUTSIDE,INSIDE,UNCERTAIN} classify(iv R) {
+    if (R.hi <  0) { return  INSIDE; }   // [-,-]
+    if (R.lo >= 0) { return OUTSIDE; }   // [+,+] or [0,+] or [0,0]
+    return UNCERTAIN;  // R.lo < 0 ≤ R.hi,  [-,+] or [-,0]
 }
 
-static float cover_subpixel(float l, float t, float r, float b,
-                            iv const R, int limit, struct iv2d_region const *region) {
-    // We may assume cover_subpixel() is only called on areas with uncertain coverage.
-    assert(coverage(R) == MAYBE);
+// Estimate coverage of a region bounded by {l,t,r,b} that's already evaluated to R,
+// using subdivision like iv2d_cover but with a limit on recursion depth.
+static float estimate_coverage(float l, float t, float r, float b,
+                               iv const R, struct iv2d_region const *region, int limit) {
+    assert(classify(R) == UNCERTAIN);
 
     if (limit == 0) {
-        // We're at our recursion limit, so we just have to guess.
-        // A basic unbiased guess is half coverage,
-        #if 0
-            return 0.5f;
-
-        // But we can do a whole lot better---some might say unreasonably so---by examining R.
-        // If negative values are inside a region, and positive outside, we'll guess the coverage
-        // is approximately the fraction of the interval that is negative.
-        //
-        // This approximation makes quality=1 entirely acceptable, and quality=2 appear to my
-        // eye almost perfect, where using 0.5 needs quality=3 or quality=4 to look good, and
-        // something like quality=7 to look almost perfect.  This is _huge_.
-        #else
-            // coverage(R) == MAYBE implies R.lo < 0 <= R.hi, so
-            //   1) R.lo        is non-zero
-            //   2) R.hi - R.lo is non-zero
-            // which together mean the divide is safe and produces a non-zero coverage estimate.
-            assert(R.lo        < 0);
-            assert(R.hi - R.lo > 0);
-            return -R.lo / (R.hi - R.lo);
-        #endif
+        // We can recurse no further, so remembering "negative inside, positive outside",
+        // we'll estimate coverage as the proportion of the interval R that is negative.
+        return -R.lo / (R.hi - R.lo);   // We know R.lo < 0 ≤ R.hi, so (R.hi - R.lo) > 0.
     }
 
-    float const x = (l + r) / 2,
-                y = (t + b) / 2;
-
+    float const x = (l+r)/2,
+                y = (t+b)/2;
     float cov = 0.0f;
     {
         iv const LT = region->fn(region, (iv){l,x}, (iv){t,y});
-        if (coverage(LT) == FULL ) { cov += 1; }
-        if (coverage(LT) == MAYBE) { cov += cover_subpixel(l,t,x,y, LT,limit-1,region); }
+        if (classify(LT) == INSIDE   ) { cov += 1; }
+        if (classify(LT) == UNCERTAIN) { cov += estimate_coverage(l,t,x,y, LT,region,limit-1); }
     }
     {
         iv const LB = region->fn(region, (iv){l,x}, (iv){y,b});
-        if (coverage(LB) == FULL ) { cov += 1; }
-        if (coverage(LB) == MAYBE) { cov += cover_subpixel(l,y,x,b, LB,limit-1,region); }
+        if (classify(LB) == INSIDE   ) { cov += 1; }
+        if (classify(LB) == UNCERTAIN) { cov += estimate_coverage(l,y,x,b, LB,region,limit-1); }
     }
     {
         iv const RT = region->fn(region, (iv){x,r}, (iv){t,y});
-        if (coverage(RT) == FULL ) { cov += 1; }
-        if (coverage(RT) == MAYBE) { cov += cover_subpixel(x,t,r,y, RT,limit-1,region); }
+        if (classify(RT) == INSIDE   ) { cov += 1; }
+        if (classify(RT) == UNCERTAIN) { cov += estimate_coverage(x,t,r,y, RT,region,limit-1); }
     }
     {
         iv const RB = region->fn(region, (iv){x,r}, (iv){y,b});
-        if (coverage(RB) == FULL ) { cov += 1; }
-        if (coverage(RB) == MAYBE) { cov += cover_subpixel(x,y,r,b, RB,limit-1,region); }
+        if (classify(RB) == INSIDE   ) { cov += 1; }
+        if (classify(RB) == UNCERTAIN) { cov += estimate_coverage(x,y,r,b, RB,region,limit-1); }
     }
-    // If the whole box has MAYBE coverage, at least one of its corners will give us _something_.
-    assert(cov > 0);
     return cov * 0.25f;
 }
 
@@ -80,22 +57,21 @@ void iv2d_cover(struct iv2d_rect   const  bounds,
               r = bounds.r,
               b = bounds.b;
     if (l < r && t < b) {
-        iv const X = {(float)l, (float)r},
-                 Y = {(float)t, (float)b},
-                 R = region->fn(region, X,Y);
-
-        if (coverage(R) == FULL) {
+        iv const R = region->fn(region, (iv){(float)l, (float)r}
+                                      , (iv){(float)t, (float)b});
+        if (classify(R) == INSIDE) {
             yield->fn(yield, bounds, 1.0f);
         }
-        if (coverage(R) == MAYBE) {
+        if (classify(R) == UNCERTAIN) {
             int const x = (l+r)/2,
                       y = (t+b)/2;
             if (l == x && t == y) {
                 if (quality > 0) {
-                    float const cov = cover_subpixel((float)l, (float)t, (float)r, (float)b,
-                                                     R, quality-1, region);
-                    assert(cov > 0);
-                    yield->fn(yield, bounds, cov);
+                    float const cov = estimate_coverage((float)l, (float)t, (float)r, (float)b,
+                                                        R, region, quality-1);
+                    if (cov > 0) {
+                        yield->fn(yield, bounds, cov);
+                    }
                 }
             } else {
                 iv2d_cover((struct iv2d_rect){l,t, x,y}, quality, region, yield);
@@ -108,15 +84,13 @@ void iv2d_cover(struct iv2d_rect   const  bounds,
 }
 
 static iv circle(struct iv2d_region const *region, iv x, iv y) {
-    struct iv2d_circle const *c = (struct iv2d_circle const*)region;
+    struct iv2d_circle const c = *(struct iv2d_circle const*)region;
 
-    return iv_sub(iv_add(iv_square(iv_sub(x, (iv){c->x, c->x})),
-                         iv_square(iv_sub(y, (iv){c->y, c->y}))),
-                  (iv){c->r*c->r, c->r*c->r});
+    return iv_sub(iv_add(iv_square(iv_sub(x, (iv){c.x, c.x})),
+                         iv_square(iv_sub(y, (iv){c.y, c.y}))),
+                  (iv){c.r*c.r, c.r*c.r});
 }
+
 struct iv2d_circle iv2d_circle(float x, float y, float r) {
-    return (struct iv2d_circle){
-        .region={circle},
-        .x=x, .y=y, .r=r,
-    };
+    return (struct iv2d_circle){.region.fn=circle, .x=x, .y=y, .r=r};
 }
