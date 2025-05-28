cf6c22027091b1efc0cbf7405f14f8329674d32f
Mike Klein <mtklein@gmail.com>
2025-05-28 19:38:40 +0800

    tweak comments

--- iv2d.h
+++ iv2d.h
@@ -9,13 +9,13 @@ typedef iv4 iv2d_region(iv4 X, iv4 Y, void const *ctx);
 
 // iv2d_circle is a sample region, a circle centered at x,y with radius r.
 //
-// The typical equation for the boundary of a circle looks like
+//   The typical equation for the boundary of a circle looks like
 //         (X - c.x)^2 + (Y - c.y)^2 == c.r^2
 //
-// so the area inside that circle is described by the inequality
+//   and the area inside that circle is described by the inequality
 //         (X - c.x)^2 + (Y - c.y)^2 <  c.r^2
 //
-// which all means this region will return the value
+//   so iv2d_circle() returns the value
 //         (X - c.x)^2 + (Y - c.y)^2 -  c.r^2
 struct iv2d_circle { float x,y,r; };
 iv2d_region iv2d_circle;
@@ -33,18 +33,14 @@ struct iv2d_rect {
 // iv2d_cover rasterizes an iv2d_region onto a bounded integer grid, yielding
 // [0,1] coverage values for rectangles within bounds that overlap the region.
 //
-// The quality parameter controls how much attention iv2d_cover pays to
-// rectangles overlapping the region's boundary.  If quality <= 0, it ignores
-// them, and so never yields rectangles with anything less than full 1.0f
-// coverage; results will be pixelated.  At quality 1, it spends minimal effort
-// estimating those partial coverages, and each higher level of quality roughly
-// doubles that effort.  I'd describe quality=1 as good, quality=2 as great,
-// and quality=4 near perfect.
-//
-// iv2d_cover yields rectangles' coverages to your callback in an unspecified
-// order, subject to change.  The rectangles may be any size, from single
-// pixels all the way up to the entire bounds.
+// The quality parameter controls how much attention iv2d_cover pays to pixels
+// overlapping the region's boundary.  If quality <= 0, it ignores them, and so
+// never yields rectangles with anything less than full 1.0f coverage; results
+// will be pixelated.  At quality 1, it spends minimal effort estimating those
+// partial coverages, and each higher level of quality roughly doubles that
+// effort.  I'd describe quality=1 as good, quality=2 as great, and quality=4
+// near perfect.
 void iv2d_cover(iv2d_region*, void const *ctx,
                 struct iv2d_rect bounds, int quality,
-                void (*yield)(struct iv2d_rect, float, void *yield_ctx), void *yield_ctx);
+                void (*yield)(struct iv2d_rect, float cov, void *yield_ctx), void *yield_ctx);
 
