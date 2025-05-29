c86ecff145758963d06af9209a179eb447d64539
Mike Klein <mtklein@gmail.com>
2025-05-29 17:39:49 +0800

    these need static or homes

--- iv.h
+++ iv.h
@@ -13,33 +13,33 @@ typedef struct {
 } iv4;
 
 
-inline iv iv_add(iv X, iv Y) {
+static inline iv iv_add(iv X, iv Y) {
     return (iv){
         X.lo + Y.lo,
         X.hi + Y.hi,
     };
 }
-inline iv4 iv4_add(iv4 X, iv4 Y) {
+static inline iv4 iv4_add(iv4 X, iv4 Y) {
     return (iv4){
         X.lo + Y.lo,
         X.hi + Y.hi,
     };
 }
 
-inline iv iv_sub(iv X, iv Y) {
+static inline iv iv_sub(iv X, iv Y) {
     return (iv){
         X.lo - Y.hi,
         X.hi - Y.lo,
     };
 }
-inline iv4 iv4_sub(iv4 X, iv4 Y) {
+static inline iv4 iv4_sub(iv4 X, iv4 Y) {
     return (iv4){
         X.lo - Y.hi,
         X.hi - Y.lo,
     };
 }
 
-inline iv iv_mul(iv X, iv Y) {
+static inline iv iv_mul(iv X, iv Y) {
     float const a = X.lo * Y.lo,
                 b = X.hi * Y.lo,
                 c = X.lo * Y.hi,
@@ -49,7 +49,7 @@ inline iv iv_mul(iv X, iv Y) {
         fmaxf(fmaxf(a,d), fmaxf(b,c)),
     };
 }
-inline iv4 iv4_mul(iv4 X, iv4 Y) {
+static inline iv4 iv4_mul(iv4 X, iv4 Y) {
     float4 const a = X.lo * Y.lo,
                  b = X.hi * Y.lo,
                  c = X.lo * Y.hi,
@@ -60,53 +60,53 @@ inline iv4 iv4_mul(iv4 X, iv4 Y) {
     };
 }
 
-inline iv iv_min(iv X, iv Y) {
+static inline iv iv_min(iv X, iv Y) {
     return (iv){
         fminf(X.lo, Y.lo),
         fminf(X.hi, Y.hi),
     };
 }
-inline iv4 iv4_min(iv4 X, iv4 Y) {
+static inline iv4 iv4_min(iv4 X, iv4 Y) {
     return (iv4){
         __builtin_elementwise_min(X.lo, Y.lo),
         __builtin_elementwise_min(X.hi, Y.hi),
     };
 }
 
-inline iv iv_max(iv X, iv Y) {
+static inline iv iv_max(iv X, iv Y) {
     return (iv){
         fmaxf(X.lo, Y.lo),
         fmaxf(X.hi, Y.hi),
     };
 }
-inline iv4 iv4_max(iv4 X, iv4 Y) {
+static inline iv4 iv4_max(iv4 X, iv4 Y) {
     return (iv4){
         __builtin_elementwise_max(X.lo, Y.lo),
         __builtin_elementwise_max(X.hi, Y.hi),
     };
 }
 
-inline iv iv_neg(iv X) {
+static inline iv iv_neg(iv X) {
     return iv_sub((iv){0}, X);
 }
-inline iv4 iv4_neg(iv4 X) {
+static inline iv4 iv4_neg(iv4 X) {
     return iv4_sub((iv4){0}, X);
 }
 
-inline iv iv_sqrt(iv X) {
+static inline iv iv_sqrt(iv X) {
     return (iv){
         sqrtf(X.lo),
         sqrtf(X.hi),
     };
 }
-inline iv4 iv4_sqrt(iv4 X) {
+static inline iv4 iv4_sqrt(iv4 X) {
     return (iv4){
         __builtin_elementwise_sqrt(X.lo),
         __builtin_elementwise_sqrt(X.hi),
     };
 }
 
-inline iv iv_square(iv X) {
+static inline iv iv_square(iv X) {
     // [+a,+b]^2 = [a^2, b^2]
     // [-a,-b]^2 = [b^2, a^2]
     // [-a,+b]^2 = [-a,+b] x [-a,+b] = [-ab, max(a^2,b^2)]
@@ -118,7 +118,7 @@ inline iv iv_square(iv X) {
                   fmaxf(a2, b2) ,
     };
 }
-inline iv4 iv4_square(iv4 X) {
+static inline iv4 iv4_square(iv4 X) {
     float4 const a2 = X.lo * X.lo,
                  ab = X.lo * X.hi,
                  b2 = X.hi * X.hi;
