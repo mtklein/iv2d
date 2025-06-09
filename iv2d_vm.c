57af75fe2c0a7e01c35c3b283d1d8f877e0a9c7d
Mike Klein <mtklein@gmail.com>
2025-06-09 16:50:51 +0800

    split builder/program insts

--- iv2d_vm.c
+++ iv2d_vm.c
@@ -2,25 +2,25 @@
 #include "len.h"
 #include <stdlib.h>
 
-typedef struct inst {
-    iv (*op)(struct inst const *ip, iv *r, iv const *v);
-    union {
-        struct { int x,y; };
-        float        imm;
-        float const *uni;
-    };
-} inst;
+typedef struct {
+    enum {X,Y,IMM,UNI,ADD,SUB,MUL,MIN,MAX,ABS,SQRT,SQUARE,RET} op;
+    int          x,y;
+    float        imm;
+    float const *uni;
+
+    //int uses, padding;
+} binst;
 
 typedef struct iv2d_builder {
-    inst *inst;
-    int   insts,padding;
+    binst *inst;
+    int    insts,padding;
 } builder;
 
 static _Bool is_pow2_or_zero(int n) {
     return 0 == (n & (n-1));
 }
 
-static int push(builder *b, inst inst) {
+static int push(builder *b, binst inst) {
     if (is_pow2_or_zero(b->insts)) {
         b->inst = realloc(b->inst, (size_t)(b->insts ? 2*b->insts : 1) * sizeof *b->inst);
     }
@@ -28,50 +28,59 @@ static int push(builder *b, inst inst) {
     return b->insts++;
 }
 
-#define next return ip[1].op(ip+1, r+1, v)
-#define op_(name) static iv op_##name(inst const *ip, iv *r, iv const *v)
-
-op_(imm) { *r = as_iv( ip->imm); next; }
-op_(uni) { *r = as_iv(*ip->uni); next; }
-int iv2d_imm(builder *b, float        imm) { return push(b, (inst){op_imm, .imm=imm}); }
-int iv2d_uni(builder *b, float const *uni) { return push(b, (inst){op_uni, .uni=uni}); }
-
 builder* iv2d_builder(void) {
     builder *b = calloc(1, sizeof *b);
-    push(b, (inst){0});  // preloaded with X
-    push(b, (inst){0});  // preloaded with Y
+    push(b, (binst){.op=X});
+    push(b, (binst){.op=Y});
     return b;
 }
 
 int iv2d_x(builder *b) { (void)b; return 0; }
 int iv2d_y(builder *b) { (void)b; return 1; }
 
-op_(min)    { *r = iv_min   (v[ip->x], v[ip->y]); next; }
-op_(max)    { *r = iv_max   (v[ip->x], v[ip->y]); next; }
+int iv2d_imm   (builder *b, float        imm) { return push(b, (binst){IMM   , .imm=imm  }); }
+int iv2d_uni   (builder *b, float const *uni) { return push(b, (binst){UNI   , .uni=uni  }); }
+int iv2d_add   (builder *b, int x, int y    ) { return push(b, (binst){ADD   , .x=x, .y=y}); }
+int iv2d_sub   (builder *b, int x, int y    ) { return push(b, (binst){SUB   , .x=x, .y=y}); }
+int iv2d_mul   (builder *b, int x, int y    ) { return push(b, (binst){MUL   , .x=x, .y=y}); }
+int iv2d_min   (builder *b, int x, int y    ) { return push(b, (binst){MIN   , .x=x, .y=y}); }
+int iv2d_max   (builder *b, int x, int y    ) { return push(b, (binst){MAX   , .x=x, .y=y}); }
+int iv2d_abs   (builder *b, int x           ) { return push(b, (binst){ABS   , .x=x      }); }
+int iv2d_sqrt  (builder *b, int x           ) { return push(b, (binst){SQRT  , .x=x      }); }
+int iv2d_square(builder *b, int x           ) { return push(b, (binst){SQUARE, .x=x      }); }
+
+int iv2d_mad(builder *b, int x, int y, int z) { return iv2d_add(b, iv2d_mul(b, x,y), z); }
+
+
+struct inst {
+    iv (*op)(struct inst const *ip, iv *r, iv const *v);
+    union {
+        struct { int x,y; };
+        float        imm;
+        float const *uni;
+    };
+};
+
+#define next return ip[1].op(ip+1, r+1, v)
+#define op_(name) static iv op_##name(struct inst const *ip, iv *r, iv const *v)
+
+op_(imm)    { *r = as_iv( ip->imm); next; }
+op_(uni)    { *r = as_iv(*ip->uni); next; }
 op_(add)    { *r = iv_add   (v[ip->x], v[ip->y]); next; }
 op_(sub)    { *r = iv_sub   (v[ip->x], v[ip->y]); next; }
 op_(mul)    { *r = iv_mul   (v[ip->x], v[ip->y]); next; }
+op_(min)    { *r = iv_min   (v[ip->x], v[ip->y]); next; }
+op_(max)    { *r = iv_max   (v[ip->x], v[ip->y]); next; }
 op_(abs)    { *r = iv_abs   (v[ip->x]          ); next; }
 op_(sqrt)   { *r = iv_sqrt  (v[ip->x]          ); next; }
 op_(square) { *r = iv_square(v[ip->x]          ); next; }
 
-int iv2d_min   (builder *b, int x, int y) { return push(b, (inst){op_min   , .x=x, .y=y}); }
-int iv2d_max   (builder *b, int x, int y) { return push(b, (inst){op_max   , .x=x, .y=y}); }
-int iv2d_add   (builder *b, int x, int y) { return push(b, (inst){op_add   , .x=x, .y=y}); }
-int iv2d_sub   (builder *b, int x, int y) { return push(b, (inst){op_sub   , .x=x, .y=y}); }
-int iv2d_mul   (builder *b, int x, int y) { return push(b, (inst){op_mul   , .x=x, .y=y}); }
-int iv2d_abs   (builder *b, int x       ) { return push(b, (inst){op_abs   , .x=x      }); }
-int iv2d_sqrt  (builder *b, int x       ) { return push(b, (inst){op_sqrt  , .x=x      }); }
-int iv2d_square(builder *b, int x       ) { return push(b, (inst){op_square, .x=x      }); }
-
-int iv2d_mad(builder *b, int x, int y, int z) {
-    return iv2d_add(b, iv2d_mul(b, x,y), z);
-}
+op_(ret) { (void)r; return v[ip->x]; }
 
 struct iv2d_program {
     struct iv2d_region region;
     int                vals, padding;
-    inst               inst[];
+    struct inst        inst[];
 };
 
 static iv run_program(struct iv2d_region const *region, iv x, iv y) {
@@ -89,31 +98,47 @@ static iv run_program(struct iv2d_region const *region, iv x, iv y) {
     return ret;
 }
 
-op_(ret) {
-    (void)r;
-    return v[ip->x];
-}
-
 struct iv2d_region* iv2d_ret(builder *b, int ret) {
+    // RET produces no value.
     int const vals = b->insts;
-    push(b, (inst){.op=op_ret, .x=ret});
+    push(b, (binst){RET, .x=ret});
 
+    // X and Y are arguments, needing no instruction to produce.
     int insts = 0;
     for (int i = 0; i < b->insts; i++) {
-        if (b->inst[i].op) {
-            insts++;
+        if (b->inst[i].op == X || b->inst[i].op == Y) {
+            continue;
         }
+        insts++;
     }
 
     struct iv2d_program *p = malloc(sizeof *p + (size_t)insts * sizeof *p->inst);
     *p = (struct iv2d_program){.region={run_program}, .vals=vals};
 
-    inst* inst = p->inst;
+    struct inst* inst = p->inst;
     for (int i = 0; i < b->insts; i++) {
-        if (b->inst[i].op) {
-            *inst++ = b->inst[i];
+        binst const *binst = b->inst+i;
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Wswitch-default"
+        switch (binst->op) {
+            case X:
+            case Y: break;
+
+            case IMM:    *inst++ = (struct inst){.op=op_imm   , .imm=binst->imm         }; break;
+            case UNI:    *inst++ = (struct inst){.op=op_uni   , .uni=binst->uni         }; break;
+            case ADD:    *inst++ = (struct inst){.op=op_add   , .x=binst->x, .y=binst->y}; break;
+            case SUB:    *inst++ = (struct inst){.op=op_sub   , .x=binst->x, .y=binst->y}; break;
+            case MUL:    *inst++ = (struct inst){.op=op_mul   , .x=binst->x, .y=binst->y}; break;
+            case MIN:    *inst++ = (struct inst){.op=op_min   , .x=binst->x, .y=binst->y}; break;
+            case MAX:    *inst++ = (struct inst){.op=op_max   , .x=binst->x, .y=binst->y}; break;
+            case ABS:    *inst++ = (struct inst){.op=op_abs   , .x=binst->x             }; break;
+            case SQRT:   *inst++ = (struct inst){.op=op_sqrt  , .x=binst->x             }; break;
+            case SQUARE: *inst++ = (struct inst){.op=op_square, .x=binst->x             }; break;
+            case RET:    *inst++ = (struct inst){.op=op_ret   , .x=binst->x             }; break;
         }
+        #pragma clang diagnostic pop
     }
+    //enum {X,Y,RET,IMM,UNI,ADD,SUB,MUL,MIN,MAX,ABS,SQRT,SQUARE} op;
 
     free(b->inst);
     free(b);
--- iv2d_vm.h
+++ iv2d_vm.h
@@ -11,12 +11,13 @@ int iv2d_y(struct iv2d_builder*);
 int iv2d_imm(struct iv2d_builder*, float);
 int iv2d_uni(struct iv2d_builder*, float const*);
 
-int iv2d_min(struct iv2d_builder*, int,int);
-int iv2d_max(struct iv2d_builder*, int,int);
 int iv2d_add(struct iv2d_builder*, int,int);
 int iv2d_sub(struct iv2d_builder*, int,int);
 int iv2d_mul(struct iv2d_builder*, int,int);
 int iv2d_mad(struct iv2d_builder*, int,int,int);
+
+int iv2d_min(struct iv2d_builder*, int,int);
+int iv2d_max(struct iv2d_builder*, int,int);
 
 int iv2d_abs   (struct iv2d_builder*, int);
 int iv2d_sqrt  (struct iv2d_builder*, int);
