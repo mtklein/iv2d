aa7bef84dfa1894dbd0a5a791051c987c2d455c1
Mike Klein <mtklein@gmail.com>
2025-06-09 17:26:48 +0800

    count uses, add a bunch of (yet unused) op variants

--- iv2d_vm.c
+++ iv2d_vm.c
@@ -7,6 +7,8 @@ typedef struct {
     int          x,y;
     float        imm;
     float const *uni;
+
+    int uses, padding;
 } binst;
 
 typedef struct iv2d_builder {
@@ -51,7 +53,7 @@ int iv2d_mad(builder *b, int x, int y, int z) { return iv2d_add(b, iv2d_mul(b, x
 
 
 struct inst {
-    iv (*op)(struct inst const *ip, iv *r, iv const *v);
+    iv (*op)(struct inst const *ip, iv *m, iv const *v, iv h);
     union {
         struct { int x,y; };
         float        imm;
@@ -59,21 +61,77 @@ struct inst {
     };
 };
 
-#define next return ip[1].op(ip+1, r+1, v)
-#define op_(name) static iv op_##name(struct inst const *ip, iv *r, iv const *v)
+#define op_(name) static iv name(struct inst const *ip, iv *m, iv const *v, iv r)
+#define next return ip[1].op(ip+1, m+1, v, r)
 
-op_(imm)    { *r = as_iv( ip->imm); next; }
-op_(uni)    { *r = as_iv(*ip->uni); next; }
-op_(add)    { *r = iv_add   (v[ip->x], v[ip->y]); next; }
-op_(sub)    { *r = iv_sub   (v[ip->x], v[ip->y]); next; }
-op_(mul)    { *r = iv_mul   (v[ip->x], v[ip->y]); next; }
-op_(min)    { *r = iv_min   (v[ip->x], v[ip->y]); next; }
-op_(max)    { *r = iv_max   (v[ip->x], v[ip->y]); next; }
-op_(abs)    { *r = iv_abs   (v[ip->x]          ); next; }
-op_(sqrt)   { *r = iv_sqrt  (v[ip->x]          ); next; }
-op_(square) { *r = iv_square(v[ip->x]          ); next; }
+op_(imm_m) { *m = as_iv(ip->imm); next; }
+op_(imm_r) {  r = as_iv(ip->imm); next; }
 
-op_(ret) { (void)r; return v[ip->x]; }
+op_(uni_m) { *m = as_iv(*ip->uni); next; }
+op_(uni_r) {  r = as_iv(*ip->uni); next; }
+
+op_(add_mx) { *m = iv_add(v[ip->x], v[ip->y]); next; }
+op_(add_mr) { *m = iv_add(       r, v[ip->y]); next; }
+op_(add_rx) {  r = iv_add(v[ip->x], v[ip->y]); next; }
+op_(add_rr) {  r = iv_add(       r, v[ip->y]); next; }
+
+op_(sub_mx) { *m = iv_sub(v[ip->x], v[ip->y]); next; }
+op_(sub_mr) { *m = iv_sub(       r, v[ip->y]); next; }
+op_(sub_rx) {  r = iv_sub(v[ip->x], v[ip->y]); next; }
+op_(sub_rr) {  r = iv_sub(       r, v[ip->y]); next; }
+
+op_(mul_mx) { *m = iv_mul(v[ip->x], v[ip->y]); next; }
+op_(mul_mr) { *m = iv_mul(       r, v[ip->y]); next; }
+op_(mul_rx) {  r = iv_mul(v[ip->x], v[ip->y]); next; }
+op_(mul_rr) {  r = iv_mul(       r, v[ip->y]); next; }
+
+op_(min_mx) { *m = iv_min(v[ip->x], v[ip->y]); next; }
+op_(min_mr) { *m = iv_min(       r, v[ip->y]); next; }
+op_(min_rx) {  r = iv_min(v[ip->x], v[ip->y]); next; }
+op_(min_rr) {  r = iv_min(       r, v[ip->y]); next; }
+
+op_(max_mx) { *m = iv_max(v[ip->x], v[ip->y]); next; }
+op_(max_mr) { *m = iv_max(       r, v[ip->y]); next; }
+op_(max_rx) {  r = iv_max(v[ip->x], v[ip->y]); next; }
+op_(max_rr) {  r = iv_max(       r, v[ip->y]); next; }
+
+op_(abs_mx) { *m = iv_abs(v[ip->x]); next; }
+op_(abs_mr) { *m = iv_abs(       r); next; }
+op_(abs_rx) {  r = iv_abs(v[ip->x]); next; }
+op_(abs_rr) {  r = iv_abs(       r); next; }
+
+op_(sqrt_mx) { *m = iv_sqrt(v[ip->x]); next; }
+op_(sqrt_mr) { *m = iv_sqrt(       r); next; }
+op_(sqrt_rx) {  r = iv_sqrt(v[ip->x]); next; }
+op_(sqrt_rr) {  r = iv_sqrt(       r); next; }
+
+op_(square_mx) { *m = iv_square(v[ip->x]); next; }
+op_(square_mr) { *m = iv_square(       r); next; }
+op_(square_rx) {  r = iv_square(v[ip->x]); next; }
+op_(square_rr) {  r = iv_square(       r); next; }
+
+op_(ret_x) { (void)m; (void)r;           return v[ip->x]; }
+op_(ret_r) { (void)m; (void)v; (void)ip; return        r; }
+
+// mx,mr,rx,rr
+static iv (*op_fn[][4])(struct inst const*, iv*, iv const*, iv) = {
+    [X] = {NULL,NULL,NULL,NULL},
+    [Y] = {NULL,NULL,NULL,NULL},
+
+    [IMM] = {imm_m,imm_m, imm_r,imm_r},  // Splatting to memory or register?
+    [UNI] = {uni_m,uni_m, uni_r,uni_r},
+
+    [ADD   ] = {   add_mx,    add_mr,    add_rx,    add_rr},
+    [SUB   ] = {   sub_mx,    sub_mr,    sub_rx,    sub_rr},
+    [MUL   ] = {   mul_mx,    mul_mr,    mul_rx,    mul_rr},
+    [MIN   ] = {   min_mx,    min_mr,    min_rx,    min_rr},
+    [MAX   ] = {   max_mx,    max_mr,    max_rx,    max_rr},
+    [ABS   ] = {   abs_mx,    abs_mr,    abs_rx,    abs_rr},
+    [SQRT  ] = {  sqrt_mx,   sqrt_mr,   sqrt_rx,   sqrt_rr},
+    [SQUARE] = {square_mx, square_mr, square_rx, square_rr},
+
+    [RET] = {ret_x,ret_r, ret_x,ret_r},  // Are we returning the hot register or v[ip->x]?
+};
 
 struct iv2d_program {
     struct iv2d_region region;
@@ -88,7 +146,7 @@ static iv run_program(struct iv2d_region const *region, iv x, iv y) {
     iv *v = (p->vals > len(small)) ? malloc((size_t)p->vals * sizeof *v) : small;
     v[0] = x;
     v[1] = y;
-    iv const ret = p->inst->op(p->inst,v+2,v);
+    iv const ret = p->inst->op(p->inst,v+2,v, as_iv(0));
 
     if (v != small) {
         free(v);
@@ -99,7 +157,14 @@ static iv run_program(struct iv2d_region const *region, iv x, iv y) {
 struct iv2d_region* iv2d_ret(builder *b, int ret) {
     // RET produces no value.
     int const vals = b->insts;
-    push(b, (binst){RET, .x=ret});
+    push(b, (binst){RET, .x=ret, .uses=1});
+
+    for (binst *binst = b->inst+b->insts; binst --> b->inst;) {
+        if (binst->uses) {
+            b->inst[binst->x].uses++;
+            b->inst[binst->y].uses++;
+        }
+    }
 
     // X and Y are arguments, needing no instruction to produce.
     int insts = 0;
@@ -115,25 +180,17 @@ struct iv2d_region* iv2d_ret(builder *b, int ret) {
 
     struct inst* inst = p->inst;
     for (binst const *binst = b->inst; binst < b->inst+b->insts; binst++) {
-        #pragma clang diagnostic push
-        #pragma clang diagnostic ignored "-Wswitch-default"
-        switch (binst->op) {
-            case X:
-            case Y: break;
 
-            case IMM:    *inst++ = (struct inst){.op=op_imm   , .imm=binst->imm         }; break;
-            case UNI:    *inst++ = (struct inst){.op=op_uni   , .uni=binst->uni         }; break;
-            case ADD:    *inst++ = (struct inst){.op=op_add   , .x=binst->x, .y=binst->y}; break;
-            case SUB:    *inst++ = (struct inst){.op=op_sub   , .x=binst->x, .y=binst->y}; break;
-            case MUL:    *inst++ = (struct inst){.op=op_mul   , .x=binst->x, .y=binst->y}; break;
-            case MIN:    *inst++ = (struct inst){.op=op_min   , .x=binst->x, .y=binst->y}; break;
-            case MAX:    *inst++ = (struct inst){.op=op_max   , .x=binst->x, .y=binst->y}; break;
-            case ABS:    *inst++ = (struct inst){.op=op_abs   , .x=binst->x             }; break;
-            case SQRT:   *inst++ = (struct inst){.op=op_sqrt  , .x=binst->x             }; break;
-            case SQUARE: *inst++ = (struct inst){.op=op_square, .x=binst->x             }; break;
-            case RET:    *inst++ = (struct inst){.op=op_ret   , .x=binst->x             }; break;
+        iv (*op)(struct inst const*, iv*, iv const*, iv)
+            = op_fn[binst->op][0];
+
+        *inst = (struct inst){.op=op, .x=binst->x, .y=binst->y};
+        if (binst->op == IMM) { inst->imm = binst->imm; }
+        if (binst->op == UNI) { inst->uni = binst->uni; }
+
+        if (inst->op) {
+            inst++;
         }
-        #pragma clang diagnostic pop
     }
 
     free(b->inst);
