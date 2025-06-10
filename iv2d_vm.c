2ee825056aea5e29cba5224076ff2c6b50e319f2
Mike Klein <mtklein@gmail.com>
2025-06-10 10:21:50 +0800

    only need slots when not writing to reg

--- iv2d_vm.c
+++ iv2d_vm.c
@@ -55,7 +55,7 @@ int iv2d_mad(builder *b, int x, int y, int z) { return iv2d_add(b, iv2d_mul(b, x
 
 
 struct inst {
-    iv (*op)(struct inst const *ip, iv *m, iv const *v, iv h);
+    iv (*op)(struct inst const *ip, iv *slot, iv const *v, iv h);
     union {
         struct { int lhs,rhs; };
         float        imm;
@@ -63,81 +63,81 @@ struct inst {
     };
 };
 
-#define op_(name) static iv name(struct inst const *ip, iv *m, iv const *v, iv reg)
-#define next return ip[1].op(ip+1, m+1, v, reg)
+#define op_(name) static iv name(struct inst const *ip, iv *slot, iv const *v, iv reg)
+#define next return ip[1].op(ip+1, slot, v, reg)
 
 // Ops are split up to 4 ways,
-//   m/r - write to *m or reg?
-//   v/r - read rhs from v or reg?
+//   s/r - write to next memory slot (*slot++) or reg?
+//   v/r - read rhs from memory slot (v[ip->rhs]) or reg?
 
-op_(imm_m) {  *m = as_iv(ip->imm); next; }
-op_(imm_r) { reg = as_iv(ip->imm); next; }
+op_(imm_s) { *slot++ = as_iv(ip->imm); next; }
+op_(imm_r) {     reg = as_iv(ip->imm); next; }
 
-op_(uni_m) {  *m = as_iv(*ip->uni); next; }
-op_(uni_r) { reg = as_iv(*ip->uni); next; }
+op_(uni_s) { *slot++ = as_iv(*ip->uni); next; }
+op_(uni_r) {     reg = as_iv(*ip->uni); next; }
 
-op_(add_mv) {  *m = iv_add(v[ip->lhs], v[ip->rhs]); next; }
-op_(add_mr) {  *m = iv_add(v[ip->lhs], reg       ); next; }
-op_(add_rv) { reg = iv_add(v[ip->lhs], v[ip->rhs]); next; }
-op_(add_rr) { reg = iv_add(v[ip->lhs], reg       ); next; }
+op_(add_sv) { *slot++ = iv_add(v[ip->lhs], v[ip->rhs]); next; }
+op_(add_sr) { *slot++ = iv_add(v[ip->lhs], reg       ); next; }
+op_(add_rv) {     reg = iv_add(v[ip->lhs], v[ip->rhs]); next; }
+op_(add_rr) {     reg = iv_add(v[ip->lhs], reg       ); next; }
 
-op_(sub_mv) {  *m = iv_sub(v[ip->lhs], v[ip->rhs]); next; }
-op_(sub_mr) {  *m = iv_sub(v[ip->lhs], reg       ); next; }
-op_(sub_rv) { reg = iv_sub(v[ip->lhs], v[ip->rhs]); next; }
-op_(sub_rr) { reg = iv_sub(v[ip->lhs], reg       ); next; }
+op_(sub_sv) { *slot++ = iv_sub(v[ip->lhs], v[ip->rhs]); next; }
+op_(sub_sr) { *slot++ = iv_sub(v[ip->lhs], reg       ); next; }
+op_(sub_rv) {     reg = iv_sub(v[ip->lhs], v[ip->rhs]); next; }
+op_(sub_rr) {     reg = iv_sub(v[ip->lhs], reg       ); next; }
 
-op_(mul_mv) {  *m = iv_mul(v[ip->lhs], v[ip->rhs]); next; }
-op_(mul_mr) {  *m = iv_mul(v[ip->lhs], reg       ); next; }
-op_(mul_rv) { reg = iv_mul(v[ip->lhs], v[ip->rhs]); next; }
-op_(mul_rr) { reg = iv_mul(v[ip->lhs], reg       ); next; }
+op_(mul_sv) { *slot++ = iv_mul(v[ip->lhs], v[ip->rhs]); next; }
+op_(mul_sr) { *slot++ = iv_mul(v[ip->lhs], reg       ); next; }
+op_(mul_rv) {     reg = iv_mul(v[ip->lhs], v[ip->rhs]); next; }
+op_(mul_rr) {     reg = iv_mul(v[ip->lhs], reg       ); next; }
 
-op_(min_mv) {  *m = iv_min(v[ip->lhs], v[ip->rhs]); next; }
-op_(min_mr) {  *m = iv_min(v[ip->lhs], reg       ); next; }
-op_(min_rv) { reg = iv_min(v[ip->lhs], v[ip->rhs]); next; }
-op_(min_rr) { reg = iv_min(v[ip->lhs], reg       ); next; }
+op_(min_sv) { *slot++ = iv_min(v[ip->lhs], v[ip->rhs]); next; }
+op_(min_sr) { *slot++ = iv_min(v[ip->lhs], reg       ); next; }
+op_(min_rv) {     reg = iv_min(v[ip->lhs], v[ip->rhs]); next; }
+op_(min_rr) {     reg = iv_min(v[ip->lhs], reg       ); next; }
 
-op_(max_mv) {  *m = iv_max(v[ip->lhs], v[ip->rhs]); next; }
-op_(max_mr) {  *m = iv_max(v[ip->lhs], reg       ); next; }
-op_(max_rv) { reg = iv_max(v[ip->lhs], v[ip->rhs]); next; }
-op_(max_rr) { reg = iv_max(v[ip->lhs], reg       ); next; }
+op_(max_sv) { *slot++ = iv_max(v[ip->lhs], v[ip->rhs]); next; }
+op_(max_sr) { *slot++ = iv_max(v[ip->lhs], reg       ); next; }
+op_(max_rv) {     reg = iv_max(v[ip->lhs], v[ip->rhs]); next; }
+op_(max_rr) {     reg = iv_max(v[ip->lhs], reg       ); next; }
 
-op_(abs_mv) {  *m = iv_abs(v[ip->rhs]); next; }
-op_(abs_mr) {  *m = iv_abs(reg       ); next; }
-op_(abs_rv) { reg = iv_abs(v[ip->rhs]); next; }
-op_(abs_rr) { reg = iv_abs(reg       ); next; }
+op_(abs_sv) { *slot++ = iv_abs(v[ip->rhs]); next; }
+op_(abs_sr) { *slot++ = iv_abs(reg       ); next; }
+op_(abs_rv) {     reg = iv_abs(v[ip->rhs]); next; }
+op_(abs_rr) {     reg = iv_abs(reg       ); next; }
 
-op_(sqrt_mv) {  *m = iv_sqrt(v[ip->rhs]); next; }
-op_(sqrt_mr) {  *m = iv_sqrt(reg       ); next; }
-op_(sqrt_rv) { reg = iv_sqrt(v[ip->rhs]); next; }
-op_(sqrt_rr) { reg = iv_sqrt(reg       ); next; }
+op_(sqrt_sv) { *slot++ = iv_sqrt(v[ip->rhs]); next; }
+op_(sqrt_sr) { *slot++ = iv_sqrt(reg       ); next; }
+op_(sqrt_rv) {     reg = iv_sqrt(v[ip->rhs]); next; }
+op_(sqrt_rr) {     reg = iv_sqrt(reg       ); next; }
 
-op_(square_mv) {  *m = iv_square(v[ip->rhs]); next; }
-op_(square_mr) {  *m = iv_square(reg       ); next; }
-op_(square_rv) { reg = iv_square(v[ip->rhs]); next; }
-op_(square_rr) { reg = iv_square(reg       ); next; }
+op_(square_sv) { *slot++ = iv_square(v[ip->rhs]); next; }
+op_(square_sr) { *slot++ = iv_square(reg       ); next; }
+op_(square_rv) {     reg = iv_square(v[ip->rhs]); next; }
+op_(square_rr) {     reg = iv_square(reg       ); next; }
 
-op_(ret_v) {           (void)m; (void)reg; return v[ip->rhs]; }
-op_(ret_r) { (void)ip; (void)m; (void)v;   return reg       ; }
+op_(ret_v) {           (void)slot; (void)reg; return v[ip->rhs]; }
+op_(ret_r) { (void)ip; (void)slot; (void)v;   return reg       ; }
 
-static iv (*op_fn[][4])(struct inst const *ip, iv *m, iv const *v, iv reg) = {
-    [IMM] = {imm_m,imm_m, imm_r,imm_r},
-    [UNI] = {uni_m,uni_m, uni_r,uni_r},
+static iv (*op_fn[][4])(struct inst const *ip, iv *slot, iv const *v, iv reg) = {
+    [IMM] = {imm_s,imm_s, imm_r,imm_r},
+    [UNI] = {uni_s,uni_s, uni_r,uni_r},
 
-    [ADD   ] = {   add_mv,    add_mr,    add_rv,    add_rr},
-    [SUB   ] = {   sub_mv,    sub_mr,    sub_rv,    sub_rr},
-    [MUL   ] = {   mul_mv,    mul_mr,    mul_rv,    mul_rr},
-    [MIN   ] = {   min_mv,    min_mr,    min_rv,    min_rr},
-    [MAX   ] = {   max_mv,    max_mr,    max_rv,    max_rr},
-    [ABS   ] = {   abs_mv,    abs_mr,    abs_rv,    abs_rr},
-    [SQRT  ] = {  sqrt_mv,   sqrt_mr,   sqrt_rv,   sqrt_rr},
-    [SQUARE] = {square_mv, square_mr, square_rv, square_rr},
+    [ADD   ] = {   add_sv,    add_sr,    add_rv,    add_rr},
+    [SUB   ] = {   sub_sv,    sub_sr,    sub_rv,    sub_rr},
+    [MUL   ] = {   mul_sv,    mul_sr,    mul_rv,    mul_rr},
+    [MIN   ] = {   min_sv,    min_sr,    min_rv,    min_rr},
+    [MAX   ] = {   max_sv,    max_sr,    max_rv,    max_rr},
+    [ABS   ] = {   abs_sv,    abs_sr,    abs_rv,    abs_rr},
+    [SQRT  ] = {  sqrt_sv,   sqrt_sr,   sqrt_rv,   sqrt_rr},
+    [SQUARE] = {square_sv, square_sr, square_rv, square_rr},
 
     [RET] = {ret_v,ret_r, ret_v,ret_r},
 };
 
 struct program {
     struct iv2d_region region;
-    int                vals, padding;
+    int                slots, padding;
     struct inst        inst[];
 };
 
@@ -145,7 +145,7 @@ static iv run_program(struct iv2d_region const *region, iv x, iv y) {
     struct program const *p = (struct program const*)region;
 
     iv small[128];
-    iv *v = (p->vals > len(small)) ? malloc((size_t)p->vals * sizeof *v) : small;
+    iv *v = (p->slots > len(small)) ? malloc((size_t)p->slots * sizeof *v) : small;
     v[0] = x;
     v[1] = y;
     iv const ret = p->inst->op(p->inst,v+2,v, x/*anything will do, this is cheapest*/);
@@ -166,7 +166,11 @@ struct iv2d_region* iv2d_ret(builder *b, int ret) {
     }
 
     struct program *p = malloc(sizeof *p + (size_t)b->insts * sizeof *p->inst);
-    *p = (struct program){.region={run_program}, .vals=b->insts};
+    *p = (struct program){.region={run_program}, .slots=2};
+
+    int *slot = calloc((size_t)b->insts, sizeof *slot);
+    slot[0] = 0;
+    slot[1] = 1;
 
     struct inst* inst = p->inst;
     _Bool rhs_in_reg = 0;
@@ -176,16 +180,19 @@ struct iv2d_region* iv2d_ret(builder *b, int ret) {
         _Bool const write_to_reg = binst->last_use == i+1
                                 && b->inst[i+1].lhs != i
                                 && b->inst[i+1].rhs == i;
+        if (!write_to_reg) {
+            slot[i] = p->slots++;
+        }
+
         // op_fn array order is
-        //   write_to_reg=0, rhs_in_reg=0 -> 0 (mv)
-        //   write_to_reg=0, rhs_in_reg=1 -> 1 (mr)
+        //   write_to_reg=0, rhs_in_reg=0 -> 0 (sv)
+        //   write_to_reg=0, rhs_in_reg=1 -> 1 (sr)
         //   write_to_reg=1, rhs_in_reg=0 -> 2 (rv)
         //   write_to_reg=1, rhs_in_reg=1 -> 3 (rr)
 
         iv (*op)(struct inst const*, iv*, iv const*, iv)
             = op_fn[binst->op][2*(int)write_to_reg + (int)rhs_in_reg];
-
-        *inst = (struct inst){.op=op, .lhs=binst->lhs, .rhs=binst->rhs};
+        *inst = (struct inst){.op=op, .lhs=slot[binst->lhs], .rhs=slot[binst->rhs]};
         if (binst->op == IMM) { inst->imm = binst->imm; }
         if (binst->op == UNI) { inst->uni = binst->uni; }
         inst++;
@@ -193,6 +200,7 @@ struct iv2d_region* iv2d_ret(builder *b, int ret) {
         rhs_in_reg = write_to_reg;
     }
 
+    free(slot);
     free(b->inst);
     free(b);
     return &p->region;
