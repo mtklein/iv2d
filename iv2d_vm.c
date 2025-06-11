c81707148731794fc2fa702fe41208d9a7ab02a7
Mike Klein <mtklein@gmail.com>
2025-06-11 19:06:18 +0800

    renames for fluency

--- iv2d_vm.c
+++ iv2d_vm.c
@@ -54,7 +54,7 @@ int iv2d_mad(builder *b, int x, int y, int z) { return iv2d_add(b, iv2d_mul(b, x
 
 struct program {
     struct iv2d_region region;
-    int         slots, padding;
+    int         stack_slots, padding;
     struct inst inst[];
 };
 
@@ -62,16 +62,16 @@ static iv run_program(struct iv2d_region const *region, iv x, iv y) {
     struct program const *p = (struct program const*)region;
 
     iv small[4096 / sizeof(iv)];
-    iv *v = (p->slots > len(small)) ? malloc((size_t)p->slots * sizeof *v) : small;
+    iv *v = (p->stack_slots > len(small)) ? malloc((size_t)p->stack_slots * sizeof *v) : small;
     v[0] = x;
-    iv rhs=y, *spill = v+1;  // the first inst will spill y to v[1].
+    iv rhs=y, *stack = v+1;  // the first inst will spill y to v[1].
 
     // TODO: try separate fall-through spill targets again; surprised that wasn't any faster.
     static void* const dispatch[] = {
         &&IMM_, &&UNI_, &&ADD_, &&SUB_, &&MUL_, &&MIN_, &&MAX_, &&ABS_, &&SQT_, &&SQR_, &&RET_
     };
     #define loop goto *dispatch[inst->op]
-    #define maybe_spill if (inst->rhs >= 0) (*spill++ = rhs, rhs = v[inst->rhs])
+    #define maybe_spill if (inst->rhs >= 0) (*stack++ = rhs, rhs = v[inst->rhs])
 
     struct inst const *inst = p->inst;
     loop;
@@ -92,18 +92,18 @@ static iv run_program(struct iv2d_region const *region, iv x, iv y) {
 struct iv2d_region* iv2d_ret(builder *b, int ret) {
     push(b, (struct inst){RET, .rhs=ret});
 
-    struct { int last_use,slot; } *meta = calloc((size_t)b->insts, sizeof *meta);
+    struct { int last_use, stack_slot; } *value = calloc((size_t)b->insts, sizeof *value);
 
     for (int i = 2; i < b->insts; i++) {
         struct inst const *binst = b->inst+i;
-        meta[binst->lhs].last_use =
-        meta[binst->rhs].last_use = i;
+        value[binst->lhs].last_use =
+        value[binst->rhs].last_use = i;
     }
 
     struct program *p = malloc(sizeof *p + ((size_t)b->insts - 2) * sizeof *p->inst);
-    *p = (struct program){.region={run_program}, .slots=2};
-    meta[0].slot = 0;
-    meta[1].slot = 1;
+    *p = (struct program){.region={run_program}};
+    value[0].stack_slot = p->stack_slots++;
+    value[1].stack_slot = p->stack_slots++;
 
     struct inst* inst = p->inst;
     _Bool rhs_in_reg = 0;
@@ -112,20 +112,20 @@ struct iv2d_region* iv2d_ret(builder *b, int ret) {
 
         *inst++ = (struct inst){
             .op  = binst->op,
-            .lhs =                   meta[binst->lhs].slot,
-            .rhs = rhs_in_reg ? -1 : meta[binst->rhs].slot,
+            .lhs =                   value[binst->lhs].stack_slot,
+            .rhs = rhs_in_reg ? -1 : value[binst->rhs].stack_slot,
             .imm = binst->imm,
             .uni = binst->uni,
         };
 
-        rhs_in_reg = (meta[i].last_use == i+1 && b->inst[i+1].lhs != i)
+        rhs_in_reg = (value[i].last_use == i+1 && b->inst[i+1].lhs != i)
                   || binst->op == RET;
         if (!rhs_in_reg) {
-            meta[i].slot = p->slots++;
+            value[i].stack_slot = p->stack_slots++;
         }
     }
 
-    free(meta);
+    free(value);
     free(b->inst);
     free(b);
     return &p->region;
