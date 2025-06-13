06e32e131151328cf1a58652d27a3b89f7f8c5c7
Mike Klein <mtklein@gmail.com>
2025-06-13 16:58:12 +0800

    basic instruction dedup

--- iv2d_vm.c
+++ iv2d_vm.c
@@ -1,6 +1,7 @@
 #include "iv2d_vm.h"
 #include "len.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct inst {
     union {
@@ -21,21 +22,96 @@ struct inst {
 static _Bool uses_lhs(struct inst const *inst) { return inst->op >= ADD; }
 static _Bool uses_rhs(struct inst const *inst) { return inst->op >= RET; }
 
+
+struct hash {
+    int vals,slots;
+    struct slot { unsigned hash; int val; } *slot;
+};
+
+static void just_insert(struct hash *h, unsigned hash, int val) {
+    unsigned const mask = (unsigned)h->slots - 1;
+    unsigned          i = hash & mask;
+    while (h->slot[i].hash) {
+        i = (i+1) & mask;
+    }
+    h->slot[i].hash = hash;
+    h->slot[i].val  = val;
+}
+static void insert(struct hash *h, unsigned hash, int val) {
+    hash = hash ? hash : 1;
+    if (h->vals/3 >= h->slots/4) {
+        struct hash grown = {.vals=h->vals, .slots=h->slots ? 2*h->slots : 2};
+        grown.slot = calloc((size_t)grown.slots, sizeof *grown.slot);
+        for (int i = 0; i < h->slots; i++) {
+            if (h->slot[i].hash) {
+                just_insert(&grown, h->slot[i].hash, h->slot[i].val);
+            }
+        }
+        free(h->slot);
+        *h = grown;
+    }
+    just_insert(h, hash, val);
+    h->vals++;
+}
+static _Bool lookup(struct hash h, unsigned hash, _Bool(*match)(int, void*), void *ctx) {
+    if (h.slot) {
+        hash = hash ? hash : 1;
+        unsigned const mask = (unsigned)h.slots - 1;
+        unsigned          i = hash & mask;
+        while (h.slot[i].hash) {
+            if (h.slot[i].hash == hash && match(h.slot[i].val, ctx)) {
+                return 1;
+            }
+            i = (i+1) & mask;
+        }
+    }
+    return 0;
+}
+
 typedef struct iv2d_builder {
     struct inst *inst;
     int          insts,padding;
+    struct hash  dedup;
 } builder;
 
+struct dedup_ctx {
+    builder     const *b;
+    struct inst const *inst;
+    int                id, padding;
+};
+static _Bool dedup(int id, void *ctx) {
+    struct dedup_ctx *dedup = ctx;
+    if (0 == memcmp(dedup->b->inst+id, dedup->inst, sizeof *dedup->inst)) {
+        dedup->id = id;
+        return 1;
+    }
+    return 0;
+}
+static unsigned fnv1a(void const *ptr, size_t len) {
+    unsigned hash = 0x811c9dc5;
+    for (unsigned char const *byte=ptr, *end=byte+len; byte != end; byte++) {
+        hash ^= *byte;
+        __builtin_mul_overflow(hash, 0x01000193, &hash);
+    }
+    return hash;
+}
 static _Bool is_pow2_or_zero(int n) {
     return 0 == (n & (n-1));
 }
-
 static int push(builder *b, struct inst inst) {
+    unsigned const hash = fnv1a(&inst, sizeof inst);
+    struct dedup_ctx dedup_ctx = {.b=b, .inst=&inst};
+    if (lookup(b->dedup, hash, dedup, &dedup_ctx)) {
+        return dedup_ctx.id;
+    }
+
     if (is_pow2_or_zero(b->insts)) {
         b->inst = realloc(b->inst, (size_t)(b->insts ? 2*b->insts : 1) * sizeof *b->inst);
     }
-    b->inst[b->insts] = inst;
-    return b->insts++;
+    int const id = b->insts++;
+    b->inst[id] = inst;
+    insert(&b->dedup, hash, id);
+    return id;
 }
 
 builder* iv2d_builder(void) {
@@ -147,6 +223,7 @@ struct iv2d_region const* iv2d_ret(builder *b, int ret) {
     p->region.scratch += sizeof(iv) * (size_t)spills;
 
     free(value);
+    free(b->dedup.slot);
     free(b->inst);
     free(b);
     return &p->region;
