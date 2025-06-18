798978a1ed499c1ad2bb826604c00d0115f5ac61
Mike Klein <mtklein@gmail.com>
2025-06-19 05:45:55 +0800

    Add fp16 iv16 support and tests

--- iv.h
+++ iv.h
@@ -3,6 +3,9 @@
 typedef float __attribute__((vector_size(16))) float4;
 typedef   int __attribute__((vector_size(16)))   int4;
 
+typedef _Float16 __attribute__((vector_size(8))) float4h;
+typedef short    __attribute__((vector_size(8)))  int4h;
+
 static inline float4 if_then_else(int4 mask, float4 t, float4 e) {
     return (float4)( ( mask & (int4)t)
                    | (~mask & (int4)e) );
@@ -92,5 +95,106 @@ static inline iv iv_inv(iv x) {
     return (iv) {
         if_then_else(x.lo > 0 | x.hi < 0 | (x.lo >= 0 & x.hi > 0), 1/x.hi, (float4){0} - 1/0.0f),
         if_then_else(x.lo > 0 | x.hi < 0 | (x.lo < 0 & x.hi <= 0), 1/x.lo, (float4){0} + 1/0.0f),
+    };
+}
+
+static inline float4h if_then_else16(int4h mask, float4h t, float4h e) {
+    return (float4h)( (mask & (int4h)t)
+                    | (~mask & (int4h)e) );
+}
+
+static inline float4h when16(int4h mask, float4h v) {
+    return if_then_else16(mask, v, (float4h){0});
+}
+
+typedef struct {
+    float4h lo, hi;
+} iv16;
+
+static inline iv16 as_iv16(_Float16 x) {
+    return (iv16){
+        (float4h){x,x,x,x},
+        (float4h){x,x,x,x},
+    };
+}
+
+static inline iv16 iv16_add(iv16 x, iv16 y) {
+    return (iv16){
+        x.lo + y.lo,
+        x.hi + y.hi,
+    };
+}
+
+static inline iv16 iv16_sub(iv16 x, iv16 y) {
+    return (iv16){
+        x.lo - y.hi,
+        x.hi - y.lo,
+    };
+}
+
+static inline iv16 iv16_mul(iv16 x, iv16 y) {
+    float4h const a = x.lo * y.lo,
+                   b = x.hi * y.lo,
+                   c = x.lo * y.hi,
+                   d = x.hi * y.hi;
+    return (iv16){
+        __builtin_elementwise_min(__builtin_elementwise_min(a,b),
+                                  __builtin_elementwise_min(c,d)),
+        __builtin_elementwise_max(__builtin_elementwise_max(a,b),
+                                  __builtin_elementwise_max(c,d)),
+    };
+}
+
+static inline iv16 iv16_mad(iv16 x, iv16 y, iv16 z) {
+    return iv16_add(iv16_mul(x,y), z);
+}
+
+static inline iv16 iv16_min(iv16 x, iv16 y) {
+    return (iv16){
+        __builtin_elementwise_min(x.lo, y.lo),
+        __builtin_elementwise_min(x.hi, y.hi),
+    };
+}
+
+static inline iv16 iv16_max(iv16 x, iv16 y) {
+    return (iv16){
+        __builtin_elementwise_max(x.lo, y.lo),
+        __builtin_elementwise_max(x.hi, y.hi),
+    };
+}
+
+static inline iv16 iv16_sqrt(iv16 x) {
+    return (iv16){
+        __builtin_elementwise_sqrt(x.lo),
+        __builtin_elementwise_sqrt(x.hi),
+    };
+}
+
+static inline iv16 iv16_square(iv16 x) {
+    float4h const a2 = x.lo * x.lo,
+                   b2 = x.hi * x.hi;
+    return (iv16){
+        when16(x.lo > 0 | x.hi < 0,
+               __builtin_elementwise_min(a2,b2)),
+                                      __builtin_elementwise_max(a2,b2),
+    };
+}
+
+static inline iv16 iv16_abs(iv16 x) {
+    float4h const a = __builtin_elementwise_abs(x.lo),
+                   b = __builtin_elementwise_abs(x.hi);
+    return (iv16){
+        when16(x.lo > 0 | x.hi < 0,
+               __builtin_elementwise_min(a,b)),
+                                      __builtin_elementwise_max(a,b),
+    };
+}
+
+static inline iv16 iv16_inv(iv16 x) {
+    return (iv16){
+        if_then_else16(x.lo > 0 | x.hi < 0 | (x.lo >= 0 & x.hi > 0),
+                       1/x.hi, (float4h){0} - 1/0.0f),
+        if_then_else16(x.lo > 0 | x.hi < 0 | (x.lo < 0 & x.hi <= 0),
+                       1/x.lo, (float4h){0} + 1/0.0f),
     };
 }
--- iv_test.c
+++ iv_test.c
@@ -7,6 +7,168 @@ static void test_add(void) {
     expect(equiv(z.hi[0], 10));
 }
 
+static void test_add16(void) {
+    iv16 z = iv16_add((iv16){{3},{4}}, (iv16){{5},{6}});
+    expect(equiv(z.lo[0],  8));
+    expect(equiv(z.hi[0], 10));
+}
+
+static void test_sub16(void) {
+    iv16 z = iv16_sub((iv16){{3},{4}}, (iv16){{5},{6}});
+    expect(equiv(z.lo[0], -3));
+    expect(equiv(z.hi[0], -1));
+}
+
+static void test_mul16(void) {
+    iv16 z = iv16_mul((iv16){{3,-3,-3,-3},{4,4,4, 4}},
+                      (iv16){{5, 5,-5,-5},{6,6,6,-1}});
+    expect(equiv(z.lo[0],  15));
+    expect(equiv(z.hi[0],  24));
+
+    expect(equiv(z.lo[1], -18));
+    expect(equiv(z.hi[1],  24));
+
+    expect(equiv(z.lo[2], -20));
+    expect(equiv(z.hi[2],  24));
+
+    expect(equiv(z.lo[3], -20));
+    expect(equiv(z.hi[3],  15));
+}
+
+static void test_mad16(void) {
+    iv16 z = iv16_mad((iv16){{1,-1,-2,-1},{2,2,2,1}},
+                      (iv16){{2,-3,-1,0},{3,3,1,0}},
+                      (iv16){{1,1,0,0},{1,1,0,0}});
+    expect(equiv(z.lo[0],  3));
+    expect(equiv(z.hi[0],  7));
+
+    expect(equiv(z.lo[1], -5));
+    expect(equiv(z.hi[1],  7));
+
+    expect(equiv(z.lo[2], -2));
+    expect(equiv(z.hi[2],  2));
+
+    expect(equiv(z.lo[3],  0));
+    expect(equiv(z.hi[3],  0));
+}
+
+static void test_min16(void) {
+    iv16 z = iv16_min((iv16){{3,-3,-3},{4,4, 4}},
+                      (iv16){{5,-5,-5},{6,6,-1}});
+    expect(equiv(z.lo[0],  3));
+    expect(equiv(z.hi[0],  4));
+
+    expect(equiv(z.lo[1], -5));
+    expect(equiv(z.hi[1],  4));
+
+    expect(equiv(z.lo[2], -5));
+    expect(equiv(z.hi[2], -1));
+}
+
+static void test_max16(void) {
+    iv16 z = iv16_max((iv16){{3,-3,-3},{4,4, 4}},
+                      (iv16){{5,-5,-5},{6,6,-1}});
+    expect(equiv(z.lo[0],  5));
+    expect(equiv(z.hi[0],  6));
+
+    expect(equiv(z.lo[1], -3));
+    expect(equiv(z.hi[1],  6));
+
+    expect(equiv(z.lo[2], -3));
+    expect(equiv(z.hi[2],  4));
+}
+
+static void test_sqrt16(void) {
+    iv16 z = iv16_sqrt((iv16){{4},{16}});
+    expect(equiv(z.lo[0], 2));
+    expect(equiv(z.hi[0], 4));
+}
+
+static void test_square16(void) {
+    {
+        iv16 z = iv16_square((iv16){{3,-3,-5},{4,4,-1}});
+        expect(equiv(z.lo[0],  9));
+        expect(equiv(z.hi[0], 16));
+
+        expect(equiv(z.lo[1],  0));
+        expect(equiv(z.hi[1], 16));
+
+        expect(equiv(z.lo[2],  1));
+        expect(equiv(z.hi[2], 25));
+    }
+
+    {
+        iv16 z = iv16_square((iv16){{0,-2,0},{4,0,0}});
+        expect(equiv(z.lo[0],  0));
+        expect(equiv(z.hi[0], 16));
+
+        expect(equiv(z.lo[1],  0));
+        expect(equiv(z.hi[1],  4));
+
+        expect(equiv(z.lo[2],  0));
+        expect(equiv(z.hi[2],  0));
+    }
+}
+
+static void test_abs16(void) {
+    {
+        iv16 z = iv16_abs((iv16){{3,-3,-5},{4,4,-1}});
+        expect(equiv(z.lo[0], 3));
+        expect(equiv(z.hi[0], 4));
+
+        expect(equiv(z.lo[1], 0));
+        expect(equiv(z.hi[1], 4));
+
+        expect(equiv(z.lo[2], 1));
+        expect(equiv(z.hi[2], 5));
+    }
+
+    {
+        iv16 z = iv16_abs((iv16){{0,-2,0},{4,0,0}});
+        expect(equiv(z.lo[0], 0));
+        expect(equiv(z.hi[0], 4));
+
+        expect(equiv(z.lo[1], 0));
+        expect(equiv(z.hi[1], 2));
+
+        expect(equiv(z.lo[2], 0));
+        expect(equiv(z.hi[2], 0));
+    }
+}
+
+static void test_inv16(void) {
+    {
+        iv16 z = iv16_inv((iv16){{+1,-4,-1,+0},
+                                 {+4,-1,+4,+0}});
+        expect(equiv(z.lo[0], 0.25));
+        expect(equiv(z.hi[0], 1   ));
+
+        expect(equiv(z.lo[1], -1.00));
+        expect(equiv(z.hi[1], -0.25));
+
+        expect(equiv(z.lo[2], -1/0.0));
+        expect(equiv(z.hi[2], +1/0.0));
+
+        expect(equiv(z.lo[3], -1/0.0));
+        expect(equiv(z.hi[3], +1/0.0));
+    }
+    {
+        iv16 z = iv16_inv((iv16){{-0,-0,-1, 0},
+                                 {-0,+0, 0,+4}});
+        expect(equiv(z.lo[0], -1/0.0));
+        expect(equiv(z.hi[0], +1/0.0));
+
+        expect(equiv(z.lo[1], -1/0.0));
+        expect(equiv(z.hi[1], +1/0.0));
+
+        expect(equiv(z.lo[2], -1/0.0));
+        expect(equiv(z.hi[2], -1    ));
+
+        expect(equiv(z.lo[3],  0.25 ));
+        expect(equiv(z.hi[3], +1/0.0));
+    }
+}
+
 static void test_sub(void) {
     iv z = iv_sub((iv){{3},{4}}, (iv){{5},{6}});
     expect(equiv(z.lo[0], -3));
@@ -164,6 +326,16 @@ static void test_inv(void) {
 }
 
 int main(void) {
+    test_add16();
+    test_sub16();
+    test_mul16();
+    test_mad16();
+    test_min16();
+    test_max16();
+    test_sqrt16();
+    test_square16();
+    test_abs16();
+    test_inv16();
     test_add();
     test_sub();
     test_mul();
