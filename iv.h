631ad33600b5fa7b1a6bf7e809286a1b77d11c19
Mike Klein <mtklein@gmail.com>
2025-06-19 06:16:11 +0800

    Add fp16 iv16 support (#9)
    * Add fp16 iv16 support and tests
    
    * Rename fp16 types and align half API
    
    * tweaks

--- iv.h
+++ iv.h
@@ -3,6 +3,9 @@
 typedef float __attribute__((vector_size(16))) float4;
 typedef   int __attribute__((vector_size(16)))   int4;
 
+typedef _Float16 __attribute__((vector_size(8))) half4;
+typedef short    __attribute__((vector_size(8))) short4;
+
 static inline float4 if_then_else(int4 mask, float4 t, float4 e) {
     return (float4)( ( mask & (int4)t)
                    | (~mask & (int4)e) );
@@ -92,5 +95,102 @@ static inline iv iv_inv(iv x) {
     return (iv) {
         if_then_else(x.lo > 0 | x.hi < 0 | (x.lo >= 0 & x.hi > 0), 1/x.hi, (float4){0} - 1/0.0f),
         if_then_else(x.lo > 0 | x.hi < 0 | (x.lo < 0 & x.hi <= 0), 1/x.lo, (float4){0} + 1/0.0f),
+    };
+}
+
+static inline half4 if_then_else16(short4 mask, half4 t, half4 e) {
+    return (half4)( ( mask & (short4)t)
+                  | (~mask & (short4)e) );
+}
+
+static inline half4 when16(short4 mask, half4 v) {
+    return if_then_else16(mask, v, (half4){0});
+}
+
+typedef struct {
+    half4 lo, hi;
+} iv16;
+
+static inline iv16 as_iv16(_Float16 x) {
+    return (iv16){
+        (half4){x,x,x,x},
+        (half4){x,x,x,x},
+    };
+}
+
+static inline iv16 iv16_add(iv16 x, iv16 y) {
+    return (iv16){
+        x.lo + y.lo,
+        x.hi + y.hi,
+    };
+}
+
+static inline iv16 iv16_sub(iv16 x, iv16 y) {
+    return (iv16){
+        x.lo - y.hi,
+        x.hi - y.lo,
+    };
+}
+
+static inline iv16 iv16_mul(iv16 x, iv16 y) {
+    half4 const a = x.lo * y.lo,
+                b = x.hi * y.lo,
+                c = x.lo * y.hi,
+                d = x.hi * y.hi;
+    return (iv16){
+        __builtin_elementwise_min(__builtin_elementwise_min(a,b),
+                                  __builtin_elementwise_min(c,d)),
+        __builtin_elementwise_max(__builtin_elementwise_max(a,b),
+                                  __builtin_elementwise_max(c,d)),
+    };
+}
+
+static inline iv16 iv16_mad(iv16 x, iv16 y, iv16 z) {
+    return iv16_add(iv16_mul(x,y), z);
+}
+
+static inline iv16 iv16_min(iv16 x, iv16 y) {
+    return (iv16){
+        __builtin_elementwise_min(x.lo, y.lo),
+        __builtin_elementwise_min(x.hi, y.hi),
+    };
+}
+
+static inline iv16 iv16_max(iv16 x, iv16 y) {
+    return (iv16){
+        __builtin_elementwise_max(x.lo, y.lo),
+        __builtin_elementwise_max(x.hi, y.hi),
+    };
+}
+
+static inline iv16 iv16_sqrt(iv16 x) {
+    return (iv16){
+        __builtin_elementwise_sqrt(x.lo),
+        __builtin_elementwise_sqrt(x.hi),
+    };
+}
+
+static inline iv16 iv16_square(iv16 x) {
+    half4 const a2 = x.lo * x.lo,
+                b2 = x.hi * x.hi;
+    return (iv16){
+        when16(x.lo > 0 | x.hi < 0, __builtin_elementwise_min(a2,b2)),
+                                    __builtin_elementwise_max(a2,b2) ,
+    };
+}
+
+static inline iv16 iv16_abs(iv16 x) {
+    half4 const a = __builtin_elementwise_abs(x.lo),
+                b = __builtin_elementwise_abs(x.hi);
+    return (iv16){
+        when16(x.lo > 0 | x.hi < 0, __builtin_elementwise_min(a,b)),
+                                    __builtin_elementwise_max(a,b) ,
+    };
+}
+
+static inline iv16 iv16_inv(iv16 x) {
+    return (iv16){
+        if_then_else16(x.lo > 0 | x.hi < 0 | (x.lo >= 0 & x.hi > 0), 1/x.hi, (half4){0} - 1/0.0f),
+        if_then_else16(x.lo > 0 | x.hi < 0 | (x.lo < 0 & x.hi <= 0), 1/x.lo, (half4){0} + 1/0.0f),
     };
 }
--- test.h
+++ test.h
@@ -4,7 +4,11 @@ int dprintf(int, char const*, ...);
 #define expect(x) if (!(x)) dprintf(2, "%s:%d %s expect(%s)\n", __FILE__, __LINE__, __func__, #x), \
                             __builtin_debugtrap()
 
-static inline _Bool equiv(float x, float y) {
+static inline __attribute__((overloadable)) _Bool equiv(float x, float y) {
     return (x <= y && y <= x)
         || (x != x && y != y);
+}
+
+static inline __attribute__((overloadable)) _Bool equiv(_Float16 x, _Float16 y) {
+    return equiv((float)x, (float)y);
 }
